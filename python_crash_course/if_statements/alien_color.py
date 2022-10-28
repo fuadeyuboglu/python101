c94fae5e4db865e645328c83861cadf0970636d4
Fuad Eyuboglu <114880492+fuadeyuboglu@users.noreply.github.com>
2022-10-28 10:17:57 +0100

    Create alien_color.py
    assigning points according to the color of alien

---
+++ python_crash_course/if_statements/alien_color.py
@@ -0,0 +1,8 @@
+alien_color = input("green, yellow or red: ").lower()
+
+if alien_color == "green":
+    print("5 points earned!")
+if alien_color == "yelow":
+    print("10 points earned!")
+if alien_color == "red":
+    print("15 points earned!")
