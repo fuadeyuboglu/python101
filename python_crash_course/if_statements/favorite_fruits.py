18ddf5dcbf60a7b65cdeb769a57873c05ba2b32a
Fuad Eyuboglu <114880492+fuadeyuboglu@users.noreply.github.com>
2022-10-28 10:36:31 +0100

    Create favorite_fruits.py
    using multiple if blocks without elif or else

---
+++ python_crash_course/if_statements/favorite_fruits.py
@@ -0,0 +1,8 @@
+favorite_fruits = ["raspberry", "banana", "melon"]
+
+if "raspberry" in favorite_fruits:
+    print("You really like rasberry!")
+if "strawberry" in favorite_fruits:
+    print("You really like strawberry!")
+if "melon" in favorite_fruits:
+    print("You really like melon!")
