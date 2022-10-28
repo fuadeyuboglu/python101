15e6c54ee1fcae1f5f63f92131a356e842d632bb
Fuad Eyuboglu <114880492+fuadeyuboglu@users.noreply.github.com>
2022-10-28 10:31:02 +0100

    Create stages_of_life.py
    In this code block, the stage of life is determined according to age.

---
+++ python_crash_course/if_statements/stages_of_life.py
@@ -0,0 +1,16 @@
+# Determining the stage of life according to age
+
+age = int(input("enter an age: "))
+
+if age < 2:
+    print("The person is a baby!")
+elif age < 4:
+    print("The person is a toddler!")
+elif age < 13:
+    print("The person is a kid!")
+elif age < 20:
+    print("The person is a teenager!")
+elif age < 65:
+    print("The person is an adult!")
+else:
+    print("The person is an elder!")
