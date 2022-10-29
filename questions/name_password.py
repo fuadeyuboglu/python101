17fe35839a946b42c4b59e25b389b00182056671
Fuad Eyuboglu <114880492+fuadeyuboglu@users.noreply.github.com>
2022-10-29 20:34:14 +0100

    Update name_password.py

--- questions/name_password.py
+++ questions/name_password.py
@@ -1,11 +1,11 @@
 # ask the user for the first name. if it matches the previously assigned name, print the password. if it doesn't, do not print the password.
 
 user = "Joseph"
-pasword = "W@12"
+password = "W@12"
 
 name = input("Please enter your first name: ")
 
 if user == name:
-    print(f"Hello {name}! The password is: {pasword}")
+    print(f"Hello {name}! The password is: {password}")
 else:
     print(f"Hello, {name}! See you later.")
