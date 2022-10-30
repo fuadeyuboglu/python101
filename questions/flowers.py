7bb0cbe180b3e7f017592a179d59123fabddd32e
Fuad Eyuboglu <114880492+fuadeyuboglu@users.noreply.github.com>
2022-10-30 13:13:03 +0000

    Create flowers.py

---
+++ questions/flowers.py
@@ -0,0 +1,30 @@
+""" 
+Fill in the blanks to complete the program that prints all elements of the flowers list below each on separate lines such as :
+
+    Rose
+    Orchid
+    Tulip         
+  
+"""
+
+flowers = ['Rose', 'Orchid', 'Tulip']
+count1 = len(....)
+count2 = ...
+
+while count...>0 :
+    print(flowers[....])
+    count... -= ...
+    count... += ...
+    
+    
+    
+# Answer:
+flowers = ['Rose', 'Orchid', 'Tulip']
+count1 = len(flowers)
+count2 = 0
+
+while count1 > 0 :
+    print(flowers[count2])
+    count1 -= 1
+    count2 += 1
+
