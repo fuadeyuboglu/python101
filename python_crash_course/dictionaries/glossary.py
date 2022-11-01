d7fb98b0b5c69c972c5063b6fdecfdea409098a5
Fuad Eyuboglu <114880492+fuadeyuboglu@users.noreply.github.com>
2022-11-01 00:40:37 +0000

    Create glossary.py

---
+++ python_crash_course/dictionaries/glossary.py
@@ -0,0 +1,11 @@
+glossary = {
+    "Operators:":"Operators are used to perform operations on variables and values.",
+    "Integer:":"Int, or integer, is a whole number, positive or negative, without decimals.",
+    "Indentation:":"Indentation refers to the spaces at the beginning of a code line.",
+    "List:":"Lists are used to store multiple items in a single variable.",
+    "Tuple:":"A tuple is a collection which is ordered and unchangeable.",
+}
+
+for key, value in glossary.items():
+    print(f"\n{key}")
+    print(f"{value}")
