1b8decf936dbae7bb2f7c6380e5815d8d5f5aa29
Fuad Eyuboglu <114880492+fuadeyuboglu@users.noreply.github.com>
2022-11-01 21:51:59 +0000

    Update favorite_languages.py

--- python_crash_course/dictionaries/favorite_languages.py
+++ python_crash_course/dictionaries/favorite_languages.py
@@ -8,5 +8,36 @@ favorite_languages = {
 language = favorite_languages["sarah"].title()
 print(f"Sarah's favorite language is {language}.")
 
+# Title
 for name, language in favorite_languages.items():
     print(f"{name.title()}'s favorite language is {language.title()}.")
+
+# Title
+for name in favorite_languages.keys():  # same output as "for name in favorite_languages:"
+    print(name.title())
+
+# Title
+friends = ["phil", "sarah"]
+for name in favorite_languages.keys():
+    print(f"Hi {name.title()}.")
+    
+    if name in friends:
+        language = favorite_languages[name].title()
+        print(f"\t{name.title()}, I see you love {language}!")
+
+if "erin" not in favorite_languages.keys():
+    print("Erin, please take our poll!")
+    
+# sorted() function
+for name in sorted(favorite_languages.keys()):
+    print(f"{name.title()}, thank you for taking the poll.")
+    
+# values() method
+print("The following languages have been mentioned:")
+for language in favorite_languages.values():
+    print(language.title())
+    
+# to see each language chosen without repetition > set
+for language in set(favorite_languages.values()):
+    print(language.title())
+    
