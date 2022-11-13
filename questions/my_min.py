93671648dfa2ea5bfdf62cd6b3f8e2c959181a1a
Fuad Eyuboglu <114880492+fuadeyuboglu@users.noreply.github.com>
2022-11-13 00:50:46 +0000

    Create my_min.py

---
+++ questions/my_min.py
@@ -0,0 +1,6 @@
+""" To improve defining function and arbitrary keyword argument skills."""
+""" Define a function named my_min to find the min of the inputted numbers."""
+
+def my_min(*numbers):
+    min_ = min(numbers)
+    return min_
