060485fb176628b10e872020ad83ec9377f51e76
Fuad Eyuboglu <114880492+fuadeyuboglu@users.noreply.github.com>
2022-11-16 11:22:39 +0000

    Create pets.py

---
+++ python_crash_course/functions/pets.py
@@ -0,0 +1,17 @@
+# Positional Arguments
+
+def describe_pet(animal_type, pet_name):
+    """Display information about a pet."""
+    print(f"\nI have a {animal_type}.")
+    print(f"My {animal_type}'s name is {pet_name.title()}.")
+    
+# Keyword Arguments
+
+describe_pet(pet_name = "Rex", animal_type = "dog")
+
+# Default Values
+
+def describe_pet(pet_name, animal_type = 'dog'):
+    """Display information about a pet."""
+    print(f"\nI have a {animal_type}.")
+    print(f"My {animal_type}'s name is {pet_name.title()}.")
