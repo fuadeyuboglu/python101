b0448e5600ded89036dde26150e3f5022439a085
Fuad Eyuboglu <114880492+fuadeyuboglu@users.noreply.github.com>
2022-11-18 23:54:36 +0000

    Update car.py

--- python_crash_course/classes/car.py
+++ python_crash_course/classes/car.py
@@ -44,4 +44,44 @@ my_new_car.read_odometer()
 # Modifying an attribute's value directly
 my_new_car.odometer_reading = 23
 my_new_car.read_odometer()
+
+
+
+# Modifying an attribute's value through a method
+class Car:
+    """A simple attempt to represent a car."""
+    
+    def __init__(self,make,model,year):
+        """Initialize attributes to describe a car."""
+        self.make = make
+        self.model = model
+        self.year = year
+        self.odometer_reading = 0
         
+    def get_descriptive_name(self):
+        """Return a neatly formatted descriptive name."""
+        long_name = f"{self.year} {self.make} {self.model}"
+        return long_name.title()
+    
+    def read_odometer(self):
+        """Print a statement showing the car's mileage."""
+        print(f"This car has {self.odometer_reading} miles on it.")
+    
+    def update_odometer(self, milage):
+        """
+        Set the odometer reading to the given value.
+        Reject the change if it attempts to roll the odometer back.
+        """
+        if milage >= self.odometer_reading:
+            self.odometer_reading = milage
+        else:
+            print("You can't roll back an odometer!")
+            
+
+my_new_car = Car('audi', 'a4', '2019')
+print(my_new_car.get_descriptive_name())
+my_new_car.update_odometer(23)
+my_new_car.read_odometer()
+
+
+#
