4692949aecc0184432f7db631cf00182da029c60
Fuad Eyuboglu <114880492+fuadeyuboglu@users.noreply.github.com>
2022-11-20 13:50:42 +0000

    Create randint.py

---
+++ python_crash_course/classes/randint.py
@@ -0,0 +1,6 @@
+""" This function takes two integer arguments 
+    and returns a randomly selected integer
+    between (and including) those numbers. """
+
+from random import randint
+randint(1,6)
